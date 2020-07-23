acd665b7bdd1082c0cea6459e2be1b437c45439b
Foneom <mugoloka@gmail.com>
2020-07-23 20:40:10 +0300

    1. Что такое итератор.[4951#329296]

--- src/main/java/ru/job4j/chapter_001/it/ArrayIt.java
+++ src/main/java/ru/job4j/chapter_001/it/ArrayIt.java
@@ -1,4 +1,37 @@
 package ru.job4j.chapter_001.it;
 
-public class ArrayIt {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
+public class ArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public ArrayIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point++];
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void forEachRemaining(Consumer<? super Integer> action) {
+
+    }
 }
--- src/main/java/ru/job4j/chapter_001/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/chapter_001/it/BackwardArrayIt.java
@@ -1,4 +1,28 @@
 package ru.job4j.chapter_001.it;
 
-public class BackwardArrayIt {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BackwardArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int index = 0;
+    private int tmp =  0;
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+        this.index = data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index >= 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index--];
+    }
 }
