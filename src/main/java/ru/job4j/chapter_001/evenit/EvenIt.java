077d4cfc8a0a8f2dbc9a7ec8bc5cb3baaf82d4d1
Foneom <mugoloka@gmail.com>
2020-07-27 20:57:09 +0300

    5.1.2. Создать итератор четные числа[150#329294] испр

--- src/main/java/ru/job4j/chapter_001/evenit/EvenIt.java
+++ src/main/java/ru/job4j/chapter_001/evenit/EvenIt.java
@@ -4,35 +4,16 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class EvenIt implements Iterator<Integer> {
-    private final int[] arr;
+    private final int[] data;
     private int index;
-    private int number;
 
-    public EvenIt(int[] arr) {
-        this.arr = arr;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-        EvenIt evenIt = new EvenIt(arr);
-        while (evenIt.hasNext()) {
-            int number = evenIt.next();
-            if (evenIt.isEven(number)) {
-                System.out.println(number);
-            }
-        }
-    }
-
-    public boolean isEven(int number) {
-        if (number % 2 == 0) {
-            return true;
-        }
-        return false;
+    EvenIt(int[] data) {
+        this.data = data;
     }
 
     @Override
     public boolean hasNext() {
-        return index < arr.length;
+        return isEven() == 0;
     }
 
     @Override
@@ -40,6 +21,21 @@ public class EvenIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return arr[index++];
+        return data[index++];
+    }
+    /**
+     * Метод поиска индекса четных элементов массива
+     * @return
+     */
+    public Integer isEven() {
+        int number = -1;
+        for (int i = index; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                index = i;
+                number++;
+                break;
+            }
+        }
+        return number;
     }
 }
