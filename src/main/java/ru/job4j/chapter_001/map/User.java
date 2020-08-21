41cdc4f4a28af1556c294c8750918262520a181a
Foneom <mugoloka@gmail.com>
2020-08-21 23:31:11 +0300

    2. Без переопределения equals и hashCode[1005#329319]

--- src/main/java/ru/job4j/chapter_001/map/User.java
+++ src/main/java/ru/job4j/chapter_001/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.chapter_001.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
 
@@ -8,4 +8,37 @@ public class User {
     private int children;
     private Calendar birthday;
 
-}
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public static void main(String[] args) {
+        User ivan = new User("Ivan", 4, Calendar.getInstance());
+        User petr = new User("Petr", 2, Calendar.getInstance());
+
+        Map<User, Object> users = new HashMap<>();
+
+        users.put(ivan, 1);
+        users.put(petr, 2);
+
+        for (Map.Entry<User, Object> user : users.entrySet()) {
+            System.out.println(user.getKey().getName() + " - " + user.getValue());
+        }
+        }
+    }
+
+
