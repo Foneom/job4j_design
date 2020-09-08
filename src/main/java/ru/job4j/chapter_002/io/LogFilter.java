27b00ce8274ad2a655eca676383ecd7bc02a2ffa
Foneom <mugoloka@gmail.com>
2020-09-08 23:13:59 +0300

    0.3. BufferedReader.[252489#329336]

---
+++ src/main/java/ru/job4j/chapter_002/io/LogFilter.java
@@ -0,0 +1,25 @@
+package ru.job4j.chapter_002.io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        List<String> line = bufferedReader.lines()
+                .filter(lin -> lin.contains("404"))
+                .limit(10)
+                .collect(Collectors.toList());
+        line.stream().forEach(System.out::println);
+        return line;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
