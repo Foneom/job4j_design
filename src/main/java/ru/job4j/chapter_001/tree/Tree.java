8222f724ecc7434e062ed1be4ba7983da102e732
Foneom <mugoloka@gmail.com>
2020-11-30 01:12:25 +0300

    1. Создать элементарную структуру дерева[1711#329304]

--- src/main/java/ru/job4j/chapter_001/tree/Tree.java
+++ src/main/java/ru/job4j/chapter_001/tree/Tree.java
@@ -23,44 +23,44 @@ public class Tree<E> implements SimpleTree<E> {
     public boolean add(E parent, E child) {
         boolean rsl = false;
         if (findBy(parent).isPresent()) {
-            findBy(parent).get().add(new Node<>(child));
-            rsl = true;
-        } else if (findBy(child).isEmpty()) {
-            root.add(new Node<>(child));
-            rsl = true;
-        }
-            return rsl;
-        }
-
-        @Override
-        public Optional<Node<E>> findBy (E value){
-            Optional<Node<E>> rsl = Optional.empty();
-            Queue<Node<E>> data = new LinkedList<>();
-            data.offer(this.root);
-            while (!data.isEmpty()) {
-                Node<E> el = data.poll();
-                if (el.value.equals(value)) {
-                    rsl = Optional.of(el);
-                    break;
-                }
-                data.addAll(el.children);
+            if (findBy(child).isPresent()) {
+            } else {
+                findBy(parent).get().add(new Node(child));
+                rsl = true;
             }
-            return rsl;
         }
-
-        public boolean isBinary() {
-        boolean result = true;
-            Queue<Node<E>> data = new LinkedList<>();
-            data.offer(this.root);
-            while (!data.isEmpty()) {
-                Node<E> el = data.poll();
-                if (el.children.size() <= 2) {
-                    data.addAll(el.children);
-                } else {
-                    result = false;
-                    break;
-                }
-            }
-            return result;
-        }
+        return rsl;
     }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
+    public boolean isBinary() {
+        boolean result = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.children.size() <= 2) {
+                data.addAll(el.children);
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
