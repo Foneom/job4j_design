7ccd8a3361ab61b608a69476ac004b118f53b6e5
Foneom <mugoloka@gmail.com>
2021-03-24 21:32:32 +0300

    5. Валидация параметров запуска.[246865#329334]

--- src/main/java/ru/job4j/chapter_002/io/Search.java
+++ src/main/java/ru/job4j/chapter_002/io/Search.java
@@ -7,10 +7,15 @@ import java.util.List;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        search(start, "").forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
+        if (!root.toFile().exists()) {
+            throw new IllegalArgumentException("Root folder is not exist.");
+        } else if (ext == null) {
+            throw new IllegalArgumentException("File extension is null");
+        }
         SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
