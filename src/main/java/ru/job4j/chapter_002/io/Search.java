d5581381007d3fa458378132f5213b663f472842
Foneom <mugoloka@gmail.com>
2021-03-26 09:50:54 +0300

    5. Валидация параметров запуска.[246865#329334] испр

--- src/main/java/ru/job4j/chapter_002/io/Search.java
+++ src/main/java/ru/job4j/chapter_002/io/Search.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
         Path start = Paths.get(args[0]);
-        search(start, args[0]).forEach(System.out::println);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
