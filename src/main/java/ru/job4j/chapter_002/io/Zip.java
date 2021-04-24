f51faffddddb89d6cfed354890fde29db912c1e0
Foneom <mugoloka@gmail.com>
2021-04-24 16:17:48 +0300

    5.2. Архивировать проект[861#329343] испр

--- src/main/java/ru/job4j/chapter_002/io/Zip.java
+++ src/main/java/ru/job4j/chapter_002/io/Zip.java
@@ -1,62 +1,76 @@
 package ru.job4j.chapter_002.io;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.zip.*;
-import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 
 public class Zip {
 
-    public void packFiles(List<Path> sources, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(target))) {
-            for (Path source : sources) {
-                if (source.toFile().isDirectory()) {
-                    packFiles(sources, target);
-                    continue;
-                }
-                try (FileInputStream fis = new FileInputStream(source.toFile())) {
-                    zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+    public void addDirectory(ZipOutputStream zout, File source, String ext) {
+        FileInputStream fis = null;
+
+        File[] files = source.listFiles();
+        System.out.println("Добавление директории: " + source.getName());
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                addDirectory(zout, files[i], ext);
+                continue;
+            }
+
+            if (!files[i].getName().endsWith(ext)) {
+                System.out.println("Добавление файла: " + files[i].getName());
+
+                try {
+                    fis = new FileInputStream(files[i]);
+
+                    zout.putNextEntry(new ZipEntry(files[i].getPath()));
+
                     byte[] buffer = new byte[fis.available()];
-                    while (fis.read(buffer) > 0)
-                        zip.write(buffer);
-                    zip.closeEntry();
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zout.write(buffer, 0, length);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        zout.closeEntry();
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
-    public List<Path> seekBy(String root, String ext) {
-        List<Path> fileList = new ArrayList<>();
-        Path path = Paths.get(root);
+    public void packZip(String source, String zip, String ext) {
+        FileOutputStream fout;
+        ZipOutputStream zout = null;
+
         try {
-            new Search().search(path, ext);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return fileList;
-    }
+            fout = new FileOutputStream(zip);
+            zout = new ZipOutputStream(fout);
+            File fileSource = new File(source);
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
+            addDirectory(zout, fileSource, ext);
+
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                zout.close();
+                System.out.println("Zip файл создан!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args) {
-        ArgsName argsName = ArgsName.of(new String[]{"-d=project.zip",
-                "-e=xml", "-o=C:\\Users\\lenovo\\IdeaProjects\\Zip"});
+        ArgsName argsName = ArgsName.of(args);
         Zip zip = new Zip();
-        List<Path> list = zip.seekBy(argsName.get("o"), argsName.get("e"));
-        zip.packFiles(list, new File(argsName.get("e")));
+        zip.packZip(argsName.get("d"), argsName.get("o"), argsName.get("e"));
     }
 }
