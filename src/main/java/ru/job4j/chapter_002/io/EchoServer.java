9adbc3a2aad063b1e8c88269495f8a0acf39e5e4
Foneom <mugoloka@gmail.com>
2021-04-28 12:44:06 +0300

    1. Бот[7921#329358]

--- src/main/java/ru/job4j/chapter_002/io/EchoServer.java
+++ src/main/java/ru/job4j/chapter_002/io/EchoServer.java
@@ -5,26 +5,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
 
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("msg=Bye")) {
-                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                            server.close();
+        public static void main(String[] args) throws IOException {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        String str;
+                        while (!(str = in.readLine()).isEmpty()) {
+                            System.out.println(str);
+                            if (str.contains("msg=Exit")) {
+                                out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                                server.close();
+                            } else if (str.contains("msg=Hello")) {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("Hello".getBytes());
+                            } else if (str.contains("msg=Any")) {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("What".getBytes());
+                            }
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 }
-
             }
         }
     }
-}
