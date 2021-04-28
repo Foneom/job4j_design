688574918959a64f8f8ece63fce9afff5e421b00
Foneom <mugoloka@gmail.com>
2021-04-28 15:02:56 +0300

    3. Slf4j - вывод переменных.[268852#329329]

--- src/main/java/ru/job4j/chapter_002/log/UsageLog4j.java
+++ src/main/java/ru/job4j/chapter_002/log/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+      char gender = 'M';
+      int age = 23;
+      byte monthOfBirth = 8;
+      short dayOfBirth = 4;
+      long ageOfBirth = 1998;
+      double height = 180.4;
+      float weight = 87.4F;
+      boolean ready = true;
+
+      LOG.debug(
+              "\nUser info \ngender: {} \nage: {} \nmonthOfBirth: {} \ndayOfBirth: {} \nageOfBirth: {} \nheight: {} \nweight: {} \nwork: {}",
+              gender, age, monthOfBirth, dayOfBirth, ageOfBirth, height, weight, ready);
     }
 }
